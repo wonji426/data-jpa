7db606cbe56ae3a8de2c1dfc087270b9c211e833
jiwon <kjw042626@gmail.com>
2025-06-20 17:24:04 +0900

    순수 JPA 기반 리포지토리 만들기

---
+++ src/main/java/study/data_jpa/repository/teamRepository.java
@@ -0,0 +1,41 @@
+package study.data_jpa.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import study.data_jpa.entity.Team;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class teamRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Team save(Team team) {
+        em.persist(team);
+        return team;
+    }
+
+    public void delete(Team team) {
+        em.remove(team);
+    }
+
+    public List<Team> findAll() {
+        return em.createQuery("select t from Team t", Team.class).getResultList();
+    }
+
+    public Optional<Team> findById(Long id) {
+        return Optional.ofNullable(em.find(Team.class, id));
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Team m", Long.class).getSingleResult();
+    }
+
+    public Team find(Long id) {
+        return em.find(Team.class, id);
+    }
+}
--- src/test/java/study/data_jpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/data_jpa/repository/MemberJpaRepositoryTest.java
@@ -8,6 +8,8 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.data_jpa.entity.Member;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,10 +26,36 @@ class MemberJpaRepositoryTest {
         Member member = new Member("memberA");
         Member saveMember = memberJpaRepository.save(member);
 
-        Member findMember = memberJpaRepository.findById(saveMember.getId());
+        Member findMember = memberJpaRepository.find(saveMember.getId());
 
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(findMember).isEqualTo(member);
+    }
+    
+    
+    @Test
+    public void basicCRUD() throws Exception {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        List<Member> members = memberJpaRepository.findAll();
+        assertThat(members.size()).isEqualTo(2);
+
+        long count = memberJpaRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+
+        long result = memberJpaRepository.count();
+        assertThat(result).isEqualTo(0);
     }
 }

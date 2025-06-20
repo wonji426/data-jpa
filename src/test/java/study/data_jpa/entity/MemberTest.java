18d1da26e15188193bdaf3bce3d627a367005782
jiwon <kjw042626@gmail.com>
2025-06-20 15:55:13 +0900

    예제 도메인 모델과 동작확인

---
+++ src/test/java/study/data_jpa/entity/MemberTest.java
@@ -0,0 +1,51 @@
+package study.data_jpa.entity;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberTest {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Test
+    public void testEntity() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member memberA = new Member("memberA", 10, teamA);
+        Member memberB = new Member("memberB", 12, teamA);
+        Member memberC = new Member("memberC", 14, teamB);
+        Member memberD = new Member("memberD", 16, teamB);
+
+        em.persist(memberA);
+        em.persist(memberB);
+        em.persist(memberC);
+        em.persist(memberD);
+
+        em.flush();
+        em.clear();
+
+        List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
+
+        for (Member member : members) {
+            System.out.println("member = " + member);
+            System.out.println("-> member.team = " + member.getTeam());
+        }
+    }
+
+
+}

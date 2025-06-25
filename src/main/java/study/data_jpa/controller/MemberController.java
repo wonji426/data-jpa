c89f1613b81750f361dddd6e220fef54f84fba97
jiwon <kjw042626@gmail.com>
2025-06-25 20:10:40 +0900

    Web 확장 - 도메인 클래스 컨버터

--- src/main/java/study/data_jpa/controller/MemberController.java
+++ src/main/java/study/data_jpa/controller/MemberController.java
@@ -2,11 +2,17 @@ package study.data_jpa.controller;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.data_jpa.dto.MemberDto;
 import study.data_jpa.entity.Member;
 import study.data_jpa.repository.MemberRepository;
+
+import java.util.Random;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,8 +32,20 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable) {
+        return memberRepository.findAll(pageable).map(MemberDto::new);
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("member1", 10));
+
+        Random random = new Random();
+        int randInt = 0;
+
+        for (int i = 0; i < 100; i++) {
+            randInt = random.nextInt(100) + 1;
+            memberRepository.save(new Member("member" + i, randInt));
+        }
     }
 }

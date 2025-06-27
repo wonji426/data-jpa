d0943d2d9d9bd4900fea345cd93e623adb23c5c1
jiwon <kjw042626@gmail.com>
2025-06-27 21:50:17 +0900

    스프링 데이터 JPA 구현체 분석 새로운 엔티티를 구별하는 방법

---
+++ src/test/java/study/data_jpa/repository/ItemRepositoryTest.java
@@ -0,0 +1,26 @@
+package study.data_jpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import study.data_jpa.entity.Item;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ItemRepositoryTest {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Test
+    public void save() throws Exception {
+        //given
+        Item item = new Item("A");
+        itemRepository.save(item);
+
+        //when
+
+        //then
+    }
+}
